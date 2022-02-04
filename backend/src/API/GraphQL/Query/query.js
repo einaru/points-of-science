497ddceef1886d4d821bfc9c19874c48c1e67956
and <andigernes@gmail.com>
2022-02-04 15:48:45 +0100

    Backend: Implemented Category Queries
    Implemented a getAllCategories query retreiving all categories available in the application.
    It uses the AllCategoriesResponseModel to return it's result back to client.
    
    Implemented a createCategory query allowing an administrator of the application to create a new category with content.
    Content is title, image, and a description.
    The category and the content is stored to database and returned as an object to the client in the response.

--- backend/src/API/GraphQL/Query/query.js
+++ backend/src/API/GraphQL/Query/query.js
@@ -4,7 +4,10 @@ import {
   AuthenticateTokenModel,
   authenticateAccessToken,
   authenticateRefreshToken,
+  AllCategoriesResponseModel,
   config,
+  categoryCreator,
+  CategoryResponseModel,
   deleteData,
   checkPermissionLevel,
   deleteRefreshTokenFromDatabase,
@@ -164,6 +167,33 @@ const getPermissionsQuery = {
   },
 };
 
+const getAllCategoriesQuery = {
+  type: AllCategoriesResponseModel,
+  args: {},
+  async resolve(parent, args, context) {
+    try {
+      const categoriesData = await getData(config.env.CATEGORY_TABLE);
+      let categories = [];
+      categoriesData.forEach((categoryData) => {
+        const category = categoryCreator();
+        categories.push(category.restoreObject(category, categoryData));
+      });
+
+      const response = getResponseObject(
+        "Categories retrieved successfully",
+        200,
+        config.env.RESPONSE_TYPE.success
+      );
+
+      categories = await Promise.all(categories);
+      response.data = categories;
+      return response;
+    } catch (error) {
+      return error;
+    }
+  },
+};
+
 // Mutation Queries - Used to update or delete data with PUT- and DELETE-requests
 const activateAccountQuery = {
   type: SignInModel,
@@ -220,6 +250,52 @@ const changePasswordQuery = {
       return getResponseObject(response, 200, config.env.RESPONSE_TYPE.success);
     } catch (error) {
       return getResponseObject(error, 400, config.env.RESPONSE_TYPE.error);
+    }
+  },
+};
+
+const createCategoryQuery = {
+  type: CategoryResponseModel,
+  args: {
+    title: { type: GraphQLString },
+    image: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+  async resolve(parent, args, context) {
+    try {
+      await authenticateAccessToken(context);
+      let response = checkPermissionLevel(
+        config.env.PERMISSION_LEVELS.ADMIN,
+        context.user
+      );
+
+      if (response.type === "error") {
+        return response;
+      }
+
+      const categoriesData = await getData(config.env.CATEGORY_TABLE);
+      const contentData = await getData(config.env.CONTENT_TABLE);
+      const categoryID = nextID(categoriesData);
+      const contentID = nextID(contentData);
+
+      const category = categoryCreator();
+      const newCategory = {
+        id: categoryID,
+      };
+      category.updateData(newCategory);
+
+      const newContent = {
+        id: contentID,
+        title: args.title,
+        image: args.image,
+        description: args.description,
+      };
+      category.content.updateData(newContent);
+      await category.content.saveData(category.content);
+      response = await category.saveData(category);
+      return response;
+    } catch (error) {
+      return error;
     }
   },
 };
@@ -412,8 +488,10 @@ export {
   authAccessTokenQuery,
   authRefreshTokenQuery,
   changePasswordQuery,
+  createCategoryQuery,
   createUserQuery,
   deleteUserQuery,
+  getAllCategoriesQuery,
   getAllUsersQuery,
   getPermissionsQuery,
   getUserByIDQuery,

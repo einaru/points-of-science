beafa38b814f812652ea7e018aeb25730f4d4ddc
Einar Uvsløkk <einar.uvslokk@gmail.com>
2022-02-05 19:19:26 +0100

    Wrap profile content in ScrollView

--- frontend/src/features/profile/Profile.jsx
+++ frontend/src/features/profile/Profile.jsx
@@ -2,7 +2,7 @@
 import { gql, useMutation } from "@apollo/client";
 import { useNavigation } from "@react-navigation/native";
 import React, { useContext, useEffect, useMemo, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { Avatar, Divider, List, Snackbar, Switch } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Clipboard from "expo-clipboard";
@@ -72,59 +72,61 @@ function Profile() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.avatarContainer}>
-        <Avatar.Text size={96} label={initials} />
-      </View>
-      <View style={styles.container}>
-        <List.Section>
-          <List.Item
-            title={user.username}
-            description={t("Username")}
-            left={() => <List.Icon icon="account" />}
-            right={() => <List.Icon icon="content-copy" />}
-            onPress={() => {
-              Clipboard.setString(user.username);
-              showSnackbar(true);
-            }}
-          />
-        </List.Section>
-        <Divider />
-        <List.Section>
-          <List.Item
-            title={t("Prefer dark theme")}
-            left={() => <List.Icon icon="theme-light-dark" />}
-            right={() => (
-              <Switch value={preferDarkTheme} onValueChange={toggleTheme} />
-            )}
-          />
-        </List.Section>
-        <Divider />
-        <List.Section>
-          <List.Item
-            title={t("Change password")}
-            left={() => <List.Icon icon="key" />}
-            right={() => <List.Icon icon="chevron-right" />}
-            onPress={() => navigation.navigate("profile:change-password")}
-          />
-          <List.Item
-            title={t("Project info")}
-            left={() => <List.Icon icon="information" />}
-            right={() => <List.Icon icon="chevron-right" />}
-            onPress={() => navigation.navigate("profile:project-info")}
-          />
-        </List.Section>
-        <Divider />
-        <List.Section>
-          <List.Item
-            title={t("Log out")}
-            left={() => <List.Icon icon="logout" />}
-            onPress={() => {
-              console.debug("Logging out");
-              logOut({ variables: { refreshToken } });
-            }}
-          />
-        </List.Section>
-      </View>
+      <ScrollView>
+        <View style={styles.avatarContainer}>
+          <Avatar.Text size={96} label={initials} />
+        </View>
+        <View style={styles.container}>
+          <List.Section>
+            <List.Item
+              title={user.username}
+              description={t("Username")}
+              left={() => <List.Icon icon="account" />}
+              right={() => <List.Icon icon="content-copy" />}
+              onPress={() => {
+                Clipboard.setString(user.username);
+                showSnackbar(true);
+              }}
+            />
+          </List.Section>
+          <Divider />
+          <List.Section>
+            <List.Item
+              title={t("Prefer dark theme")}
+              left={() => <List.Icon icon="theme-light-dark" />}
+              right={() => (
+                <Switch value={preferDarkTheme} onValueChange={toggleTheme} />
+              )}
+            />
+          </List.Section>
+          <Divider />
+          <List.Section>
+            <List.Item
+              title={t("Change password")}
+              left={() => <List.Icon icon="key" />}
+              right={() => <List.Icon icon="chevron-right" />}
+              onPress={() => navigation.navigate("profile:change-password")}
+            />
+            <List.Item
+              title={t("Project info")}
+              left={() => <List.Icon icon="information" />}
+              right={() => <List.Icon icon="chevron-right" />}
+              onPress={() => navigation.navigate("profile:project-info")}
+            />
+          </List.Section>
+          <Divider />
+          <List.Section>
+            <List.Item
+              title={t("Log out")}
+              left={() => <List.Icon icon="logout" />}
+              onPress={() => {
+                console.debug("Logging out");
+                logOut({ variables: { refreshToken } });
+              }}
+            />
+          </List.Section>
+        </View>
+      </ScrollView>
       <Snackbar
         visible={visibleSnackbar}
         duration={5000}

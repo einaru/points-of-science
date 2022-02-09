f095a8249e4ffd6b53db6105e5229f61a440a4f4
and <andigernes@gmail.com>
2022-02-09 11:06:07 +0100

    Backend: Utility got New Functions and Refactor
    Implemented a new getDataFromDatabaseByFilter function retrieving data from database by using a firestore filter type.
    
    Implemented a restoreChallenges function restoring challenge objects by using a list of challenge ids.
    The challenge object is restored from an id to have data and available functions.
    
    Refactored out all objects data for their stored format into own functions. An example is getCategoryStoredObject, which return the format a category should have when storing to database.
    These functions makes it easier to store nested objects as they could replace the format of other objects they reference to stored versions.
    
    The same structure is made for response objects as well, allowing objects to replace references with object structures of that object type it references.

--- backend/src/BusinessLogic/Util/utility.js
+++ backend/src/BusinessLogic/Util/utility.js
@@ -1,4 +1,10 @@
-import { updateData } from "../../internal.js";
+import {
+  challengeCreator,
+  getData,
+  getFilter,
+  getDataByFilter,
+  updateData,
+} from "../../internal.js";
 import config from "../../Config/config.js";
 
 function createObjectTemplate(functionKey, code) {
@@ -16,43 +22,156 @@ function getResponseObject(message, statusCode, type, data) {
   };
 }
 
+function getDataFromDatabaseByFilter(id, table) {
+  const filter = getFilter({
+    key: "id",
+    operator: "==",
+    value: id,
+  });
+  return getDataByFilter(table, filter);
+}
+
+function restoreChallenges(challengeList) {
+  return new Promise((resolve, reject) => {
+    getData(config.db.table.challenge)
+      .then((data) => {
+        const challengesData = data.filter((challenge) => {
+          return challengeList.includes(challenge.id);
+        });
+
+        const challenges = [];
+        challengesData.forEach((challengeData) => {
+          challenges.push(
+            new Promise((resolve, reject) => {
+              const challenge = challengeCreator();
+              challenge
+                .restoreObject(challenge, challengeData)
+                .then((object) => {
+                  resolve(challenge);
+                })
+                .catch((error) => {
+                  reject(error);
+                });
+            })
+          );
+        });
+
+        if (!challenges.length) {
+          resolve(null);
+        } else {
+          resolve(Promise.all(challenges));
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function getDataFromStoredObject(list, functionToCall) {
+  const data = [];
+  list.forEach((object) => {
+    const { id } = functionToCall(object);
+    data.push(id);
+  });
+
+  return data;
+}
+
+function getChallengeStoredObject(object) {
+  return {
+    id: object.data.id,
+    categoryID: object.data.categoryID,
+    difficulty: object.data.difficulty,
+    contentID: object.content.data.id,
+    reflectionID: object.reflection.data.id,
+    rewardID: object.reward.data.id,
+  };
+}
+
+function getCategoryStoredObject(object) {
+  return {
+    id: object.data.id,
+    challenges: getDataFromStoredObject(
+      object.data.challenges,
+      getChallengeStoredObject
+    ),
+    contentID: object.content.data.id,
+    progressID: object.content.data.id,
+  };
+}
+
+function getContentStoredObject(object) {
+  return {
+    id: object.data.id,
+    title: object.data.title,
+    image: object.data.image,
+    description: object.data.description,
+  };
+}
+
 function convertToStoredObject(key, object) {
   switch (key) {
     case "category":
-      return {
-        id: object.data.id,
-        challenges: object.data.challenges,
-        contentID: object.content.data.id,
-        progressID: object.content.data.id,
-      };
+      return getCategoryStoredObject(object);
+    case "challenge":
+      return getChallengeStoredObject(object);
     case "content":
-      return {
-        id: object.data.id,
-        title: object.data.title,
-        image: object.data.image,
-        description: object.data.description,
-      };
+      return getContentStoredObject(object);
     default:
       return {};
   }
 }
 
+function getDataFromResponseObject(list, functionToCall) {
+  const data = [];
+  list.forEach((object) => {
+    data.push(functionToCall(object));
+  });
+
+  return data;
+}
+
+function getChallengeResponseObject(object) {
+  return {
+    id: object.data.id,
+    categoryID: object.data.categoryID,
+    difficulty: object.data.difficulty,
+    ...object.content.data,
+    reflection: object.reflection.data,
+    reward: object.reward.data,
+  };
+}
+
+function getCategoryResponseObject(object) {
+  return {
+    id: object.data.id,
+    challenges: getDataFromResponseObject(
+      object.data.challenges,
+      getChallengeResponseObject
+    ),
+    ...object.content.data,
+    progress: object.progress.data,
+  };
+}
+
+function getContentResponseObject(object) {
+  return {
+    id: object.data.id,
+    title: object.data.title,
+    image: object.data.image,
+    description: object.data.description,
+  };
+}
+
 function convertToResponseObject(key, object) {
   switch (key) {
     case "category":
-      return {
-        id: object.data.id,
-        challenge: object.data.challenges,
-        ...object.content.data,
-        progress: object.progress.data,
-      };
+      return getCategoryResponseObject(object);
+    case "challenge":
+      return getChallengeResponseObject(object);
     case "content":
-      return {
-        id: object.data.id,
-        title: object.data.title,
-        image: object.data.image,
-        description: object.data.description,
-      };
+      return getContentResponseObject(object);
     default:
       return {};
   }
@@ -88,5 +207,7 @@ export {
   convertToStoredObject,
   convertToResponseObject,
   getResponseObject,
+  getDataFromDatabaseByFilter,
   saveData,
+  restoreChallenges,
 };

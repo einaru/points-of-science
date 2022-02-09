8553660a193f12a4999c9549516716fc26204204
and <andigernes@gmail.com>
2022-02-09 11:17:58 +0100

    Backend: Implemented Functions for Challenge
    The updte method for challenge checks for null and if input is an object.
    
    Removed createObjectTemplate function and replaced it with a function in utility.js intstead for more reuse.
    
    Implemented a restoreObject function in challenge.js.
    The function populates the challenge object and all its sub-objects with data.
    
    Implemented the deleteChallenge function, which removes the challenge from a category and deletes the challenge from the database.
    
    Implemented an addReflection function allowing a user to specify which type of reflection they want to use in a challenge.
    This could be set when a user creates a challenge.
    
    Updated ids in object to be a string instead of an int.

--- backend/src/BusinessLogic/Content/challenge.js
+++ backend/src/BusinessLogic/Content/challenge.js
@@ -1,9 +1,15 @@
 import {
   contentCreator,
+  createObjectTemplate,
+  convertToResponseObject,
+  deleteData,
+  getDataFromDatabaseByFilter,
   reflectionCreator,
   argumentCreator,
   rewardCreator,
+  saveData,
 } from "../../internal.js";
+import config from "../../Config/config.js";
 
 const DifficultyEnum = Object.freeze({
   beginner: 1,
@@ -11,69 +17,141 @@ const DifficultyEnum = Object.freeze({
   expert: 3,
 });
 
-function challengeCreator(category) {
+function updateChallenge(challenge) {
+  const functionKey = "updateData";
+  const code = (args) => {
+    if (args == null || args !== Object(args)) {
+      throw new Error(
+        "Challenge could not be updated because of wrong type of input. Input must be an object."
+      );
+    }
+
+    Object.keys(args).forEach((key) => {
+      challenge[key] = args[key];
+    });
+  };
+
+  return createObjectTemplate(functionKey, code);
+}
+
+function restoreObject() {
+  const functionKey = "restoreObject";
+  const code = (challenge, challengeData) => {
+    return new Promise((resolve, reject) => {
+      const { contentID, reflectionID, rewardID } = challengeData;
+
+      const contentData = getDataFromDatabaseByFilter(
+        contentID,
+        config.db.table.content
+      );
+
+      const reflectionData = getDataFromDatabaseByFilter(
+        reflectionID,
+        config.db.table.reflection
+      );
+
+      const rewardData = getDataFromDatabaseByFilter(
+        rewardID,
+        config.db.table.reward
+      );
+
+      Promise.all([contentData, reflectionData, rewardData])
+        .then((data) => {
+          challenge.updateData(challengeData);
+
+          let content = [];
+          let reflection = [];
+          let reward = [];
+          if (data[0] != null) {
+            content = data[0][0];
+            challenge.content.updateData(content);
+          }
+
+          if (data[1] != null) {
+            reflection = data[1][0];
+            challenge.reflection.updateData(reflection);
+          }
+
+          if (data[2] != null) {
+            reward = data[2][0];
+            challenge.reward.updateData(reward);
+          }
+
+          resolve(convertToResponseObject("challenge", challenge));
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
+  return createObjectTemplate(functionKey, code);
+}
+
+function deleteChallenge(challenge) {
+  const key = "deleteChallenge";
+  const code = (category) => {
+    return new Promise((resolve, reject) => {
+      if (category == null || category !== Object(category)) {
+        reject(
+          Error(
+            "Category to delete this challenge from has wrong type. Input must be an object."
+          )
+        );
+      }
+
+      category.removeChallenge(challenge);
+      deleteData(config.db.table.challenge, challenge.data.id)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
+  return createObjectTemplate(key, code);
+}
+
+function addReflectionType(reflectionType) {
+  if (reflectionType) {
+    return reflectionCreator();
+  }
+
+  return argumentCreator();
+}
+
+function emptyData() {
+  return {
+    data: {
+      id: "",
+      categoryID: "",
+      difficulty: DifficultyEnum.beginner,
+      difficultyLevels: DifficultyEnum,
+    },
+  };
+}
+
+function challengeCreator(reflectionType) {
   const content = contentCreator();
   // const activity =
 
-  // Må definere hvordan en challenge vet om den skal opprette en fri refleksjon eller et argument.
-  const reflection = reflectionCreator();
+  const reflection = addReflectionType(reflectionType);
 
   const reward = rewardCreator();
   const challenge = emptyData();
 
   return {
     ...content,
-    ...activity,
+    // ...activity,
     ...reflection,
     ...reward,
-    ...category,
     ...challenge,
-    ...addChallenge(challenge.data),
-    ...removeChallenge(challenge.data),
+    ...updateChallenge(challenge.data),
+    ...restoreObject(),
     ...deleteChallenge(challenge.data),
-  };
-}
-
-function addChallenge(challenge) {
-  const key = "addChallenge";
-  const code = (challenge) => {
-    // Fill in the blanks
-  };
-
-  return createObjectTemplate(key, code);
-}
-
-function removeChallenge(challenge) {
-  const key = "removeChallenge";
-  const code = (challenge) => {
-    // Fill in the blanks
-  };
-
-  return createObjectTemplate(key, code);
-}
-
-function deleteChallenge(challenge) {
-  const key = "deleteChallenge";
-  const code = () => {
-    // Fill in the blanks
-  };
-
-  return createObjectTemplate(key, code);
-}
-
-function createObjectTemplate(functionKey, code) {
-  const object = {};
-  object[functionKey] = code;
-  return object;
-}
-
-function emptyData() {
-  return {
-    data: {
-      id: 0,
-      difficulty: DifficultyEnum.beginner,
-      difficulty_levels: DifficultyEnum,
-    },
+    ...saveData(),
   };
 }
 

976889b6dcbf84fbc41db01a92c01ecb1a6f529b
and <andigernes@gmail.com>
2022-02-14 09:55:05 +0100

    Backend: Connect to Firestore Emulator during Test
    Added a check in the test case, checking if the Firestore variable is present.
    If it is present the test will use Firestore as database to test against.

--- backend/src/Database/Connection/connection.js
+++ backend/src/Database/Connection/connection.js
@@ -21,12 +21,17 @@ function connectToDatabase() {
   const env = process.env.NODE_ENV;
   switch (env) {
     case "test":
-      database = connectToNonProductionDatabase(
-        config.db.test.folder,
-        config.db.test.file,
-        config.db.test.data
-      );
-      provider = JSONProvider;
+      if (config.db.test.firestore != null && config.db.test.firestore) {
+        database = connectToFirebaseEmulator();
+        provider = FirebaseProvider;
+      } else {
+        database = connectToNonProductionDatabase(
+          config.db.test.folder,
+          config.db.test.file,
+          config.db.test.data
+        );
+        provider = JSONProvider;
+      }
       break;
     case "development":
       database = connectToFirebaseEmulator();
@@ -50,8 +55,7 @@ function connectToNonProductionDatabase(
     makeFolders(databaseFolder);
     filePathToDatabase = path.resolve(here, filePath);
     copyDummyDataToDatabase(dummyDataPath);
-    const dummyData = JSON.parse(fs.readFileSync(filePathToDatabase, "utf-8"));
-    return dummyData;
+    return JSON.parse(fs.readFileSync(filePathToDatabase, "utf-8"));
   } catch (error) {
     console.log(
       `\n[-] An error occurred in file connection.js when connecting to database. Error: ${error.message}.`

7c1c29df6e13344b3a68dd42c8cedbd1e85d42b6
and <andigernes@gmail.com>
2022-02-15 09:27:39 +0100

    Backend: Refactor Challenge Query
    Refactored out code from create challenge query to a new utility file located in src/API/GraphQL/Query/Util.
    The refactored code in utility creates and returns a challenge, its content, and the reward.
    The code saving the content, challenge, reward, and category is also refactored out to the same file.
    
    The create challenge query now also takes as input a reward, and creates a reward object when a challenge is created.

---
+++ backend/src/API/GraphQL/Query/Util/utility.js
@@ -0,0 +1,86 @@
+import { challengeCreator, nextID } from "../../../../internal.js";
+import config from "../../../../Config/config.js";
+
+function createContent(content, title, image, description) {
+  const contentID = nextID(config.db.table.content);
+  const newContent = {
+    id: contentID,
+    title,
+    image,
+    description,
+  };
+
+  content.updateData(newContent);
+  return content;
+}
+
+function createChallenge(categoryID, difficulty) {
+  const id = nextID(config.db.table.challenge);
+  const challenge = challengeCreator();
+  const newChallenge = {
+    id,
+    categoryID,
+    difficulty: difficulty || 1,
+  };
+
+  challenge.updateData(newChallenge);
+  return challenge;
+}
+
+function saveChallenge(content, category, challenge, reward) {
+  return new Promise((resolve, reject) => {
+    content
+      .saveData(
+        "content",
+        content,
+        config.db.table.content,
+        "Content stored successfully."
+      )
+      .then((response) => {
+        return reward.saveData(
+          "reward",
+          reward,
+          config.db.table.reward,
+          "Reward stored successfully"
+        );
+      })
+      .then((response) => {
+        category.addChallenge(challenge);
+        return category.saveData(
+          "category",
+          category,
+          config.db.table.category,
+          "Category stored successfully."
+        );
+      })
+      .then((response) => {
+        return challenge.saveData(
+          "challenge",
+          challenge,
+          config.db.table.challenge,
+          "Challenge stored successfully."
+        );
+      })
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function createReward(reward, maxPoints, firstTryPoints, bonusPoints) {
+  const id = nextID(config.db.table.reward);
+  const newReward = {
+    id,
+    maxPoints,
+    firstTryPoints,
+    bonusPoints,
+  };
+
+  reward.updateData(newReward);
+  return reward;
+}
+
+export { createContent, createChallenge, createReward, saveChallenge };

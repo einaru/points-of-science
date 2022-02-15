1856cb926bed2e12a915266d07da05a17e7b91ab
and <andigernes@gmail.com>
2022-02-15 13:20:01 +0100

    Backend: Refactor Reward Query
    Refactored reward query to throw AuthenticationError and ForbiddenError.

--- backend/src/API/GraphQL/Query/rewardQuery.js
+++ backend/src/API/GraphQL/Query/rewardQuery.js
@@ -1,12 +1,11 @@
 import { GraphQLString } from "graphql";
 import {
   NormalResponseModel,
-  authenticateAccessToken,
   challengeCreator,
-  checkPermissionLevel,
   getDataFromDatabaseByFilter,
 } from "../../../internal.js";
 import config from "../../../Config/config.js";
+import { AuthenticationError, ForbiddenError } from "../error.js";
 
 function getResponseObject(message, statusCode, type) {
   return {
@@ -23,57 +22,53 @@ const deleteRewardQuery = {
     rewardID: { type: GraphQLString },
   },
   async resolve(parent, args, context) {
-    try {
-      await authenticateAccessToken(context);
-      const { user } = context;
-
-      let response = checkPermissionLevel(config.permissionLevel.admin, user);
-      if (response.type === "error") {
-        return response;
-      }
-
-      const challengesData = await getDataFromDatabaseByFilter(
-        "rewardID",
-        args.rewardID,
-        config.db.table.challenge
-      );
-
-      if (challengesData == null || !challengesData.length) {
-        return getResponseObject(
-          "The reward you try to delete does not exist.",
-          400,
-          config.responseType.error
-        );
-      }
-
-      challengesData.forEach((challengeData) => {
-        const challenge = challengeCreator();
-        challenge
-          .restoreObject(challenge, challengeData)
-          .then(() => {
-            return challenge.reward.deleteReward(challenge);
-          })
-          .then(() => {
-            challenge.saveData(
-              "challenge",
-              challenge,
-              config.db.table.challenge,
-              "Challenge stored successfully."
-            );
-          })
-          .catch((error) => {
-            throw new Error(error);
-          });
-      });
-
-      return getResponseObject(
-        `Reward successfully deleted.`,
-        200,
-        config.responseType.success
-      );
-    } catch (error) {
-      return error;
+    if (!context.user) {
+      throw new AuthenticationError("User is not authorized.");
     }
+
+    if (context.user.permission !== config.permissionLevel.admin) {
+      throw new ForbiddenError("Admin permission is required.");
+    }
+
+    const challengesData = await getDataFromDatabaseByFilter(
+      "rewardID",
+      args.rewardID,
+      config.db.table.challenge
+    );
+
+    if (challengesData == null || !challengesData.length) {
+      return getResponseObject(
+        "The reward you try to delete does not exist.",
+        400,
+        config.responseType.error
+      );
+    }
+
+    challengesData.forEach((challengeData) => {
+      const challenge = challengeCreator();
+      challenge
+        .restoreObject(challenge, challengeData)
+        .then(() => {
+          return challenge.reward.deleteReward(challenge);
+        })
+        .then(() => {
+          challenge.saveData(
+            "challenge",
+            challenge,
+            config.db.table.challenge,
+            "Challenge stored successfully."
+          );
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+    });
+
+    return getResponseObject(
+      `Reward successfully deleted.`,
+      200,
+      config.responseType.success
+    );
   },
 };
 

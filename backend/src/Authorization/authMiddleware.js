2e055cf0fb9bbb2c48db7aa7b5fe86bb3404160a
Einar Uvsløkk <einaru@stud.ntnu.no>
2022-02-15 11:08:49 +0100

    Fix errors in authMiddleware

--- backend/src/Authorization/authMiddleware.js
+++ backend/src/Authorization/authMiddleware.js
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
-import config from "../Config/config";
+import config from "../Config/config.js";
 
 export default function authMiddleware(req, _, next) {
   req.user = null;
   if (req.headers.authorization) {
-    const [scheme, token] = req.headers.authorization;
+    const [scheme, token] = req.headers.authorization.split(" ");
     if (scheme === "Bearer" && token) {
       jwt.verify(token, config.secret.accessToken, (error, payload) => {
         if (!error) {
@@ -12,6 +12,6 @@ export default function authMiddleware(req, _, next) {
         }
       });
     }
-    next();
   }
+  next();
 }

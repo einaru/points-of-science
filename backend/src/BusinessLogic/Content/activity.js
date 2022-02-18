5a4d5c00b5ad55e7bb9c98d23317b4b94db46d7d
and <andigernes@gmail.com>
2022-02-18 11:09:21 +0100

    Backend: Implemented Activity
    Updated empty data function for activity to have an ID as string, a type defining if activity is in app or have to be done outside of application, a description summarizing how to perform the activity step by step.
    
    Implemented set id function updating the id of the activity.
    
    Implemented a set type function updating the type of an activity.
    
    Implemented a set description updating the description of an activity.
    
    Implemented add and remove function adding or removing an element to or from the input list.
    
    Implemented a delete activity function deleting an activity from a challenge and in database.

--- backend/src/BusinessLogic/Content/activity.js
+++ backend/src/BusinessLogic/Content/activity.js
@@ -1,3 +1,120 @@
+import { createObjectTemplate, deleteData, saveData } from "../../internal.js";
+import config from "../../Config/config.js";
+import { assertTextInput } from "../../API/GraphQL/assert.js";
+
+function emptyData() {
+  return {
+    data: {
+      id: "",
+      type: 1,
+      description: "",
+      hints: [],
+      resources: [],
+    },
+  };
+}
+
+function setID(activity) {
+  const functionKey = "setID";
+  const code = (id) => {
+    if (id == null) {
+      throw new Error("Activity ID is missing.");
+    }
+
+    activity.id = id;
+  };
+
+  return createObjectTemplate(functionKey, code);
+}
+
+function setType(activity) {
+  const functionKey = "setType";
+  const code = (type) => {
+    if (type == null) {
+      throw new Error("Type is missing.");
+    }
+
+    activity.type = type;
+  };
+
+  return createObjectTemplate(functionKey, code);
+}
+
+function setDescription(activity) {
+  const functionKey = "setDescription";
+  const code = (description) => {
+    assertTextInput(description, "Activity description");
+
+    activity.description = description;
+  };
+
+  return createObjectTemplate(functionKey, code);
+}
+
+function add() {
+  const functionKey = "add";
+  const code = (list, element) => {
+    if (!Array.isArray(list)) {
+      throw new Error("The list to add an element to is not an Array.");
+    }
+
+    if (!list.includes(element)) {
+      list.push(element);
+    }
+  };
+
+  return createObjectTemplate(functionKey, code);
+}
+
+function remove() {
+  const functionKey = "remove";
+  const code = (list, element) => {
+    if (!Array.isArray(list)) {
+      throw new Error("The list to add an element to is not an Array.");
+    }
+
+    const position = list.indexOf(element);
+    if (position > -1) {
+      list.splice(position, 1);
+    }
+  };
+
+  return createObjectTemplate(functionKey, code);
+}
+
+function deleteActivity(activity) {
+  const functionKey = "deleteActivity";
+  const code = (challenge) => {
+    return new Promise((resolve, reject) => {
+      if (challenge == null || challenge !== Object(challenge)) {
+        reject(
+          Error(
+            "Challenge to delete this activity from has wrong type. Input must be an object."
+          )
+        );
+      }
+
+      deleteData(config.db.table.activity, activity.id)
+        .then((response) => {
+          const emptyActivity = emptyData();
+          challenge.activity.setID(emptyActivity.data.id);
+          challenge.activity.setDescription(emptyActivity.data.description);
+          challenge.activity.setType(emptyActivity.data.type);
+          const { hints, resources } = challenge.activity.data;
+          hints.splice(0, hints.length);
+          resources.splice(0, resources.length);
+
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
+  return createObjectTemplate(functionKey, code);
+}
+
 function activityCreator() {
   const activity = emptyData();
 
@@ -5,61 +122,12 @@ function activityCreator() {
     activity: {
       ...activity,
       ...setID(activity.data),
-      ...add(activity.data),
-      ...remove(activity.data),
+      ...setType(activity.data),
+      ...setDescription(activity.data),
+      ...add(),
+      ...remove(),
       ...deleteActivity(activity.data),
-    },
-  };
-}
-
-function setID(activity) {
-  const key = "setID";
-  const code = (id) => {
-    // Fill in the blanks
-  };
-
-  return createObjectTemplate(key, code);
-}
-
-function add(activity) {
-  const key = "add";
-  const code = (list, element) => {
-    // Fill in the blanks
-  };
-
-  return createObjectTemplate(key, code);
-}
-
-function remove(activity) {
-  const key = "remove";
-  const code = (list, element) => {
-    // Fill in the blanks
-  };
-
-  return createObjectTemplate(key, code);
-}
-
-function deleteActivity(activity) {
-  const key = "deleteActivity";
-  const code = () => {
-    // Fill in the blanks
-  };
-
-  return createObjectTemplate(key, code);
-}
-
-function createObjectTemplate(functionKey, code) {
-  const object = {};
-  object[functionKey] = code;
-  return object;
-}
-
-function emptyData() {
-  return {
-    data: {
-      id: 0,
-      hints: [],
-      resources: [],
+      ...saveData(),
     },
   };
 }

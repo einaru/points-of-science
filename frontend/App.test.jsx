b67446d14ad48fd7bf3098b3c7c695310d28b400
Einar Uvsløkk <einar.uvslokk@gmail.com>
2022-02-22 16:19:23 +0100

    Update App test to account for added LoadingScreen
    The LoadingScreen was added to the App component with commit
    574f2bf7480792cf373a50a8e6cd88a0fa6d1896.

--- frontend/App.test.jsx
+++ frontend/App.test.jsx
@@ -6,8 +6,8 @@ import App from "./App";
 jest.useFakeTimers();
 
 describe("<App />", () => {
-  it("has 1 child", () => {
+  it("has 2 children", () => {
     const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
+    expect(tree.children.length).toBe(2);
   });
 });

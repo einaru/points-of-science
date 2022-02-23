4e222d741eb566216abb5272b0dc862bf86cd9a0
Einar Uvsløkk <einaru@stud.ntnu.no>
2022-02-23 10:31:59 +0100

    Implement a smiley-o-meter
    The smiley-o-meter is inspired by the [Fun Toolkit][] by Janet C. Read.
    It can be used to collect data as a momentary assessment in the app.
    Currently implemented in the last step of a challenge to let users rate
    a given challenge upon completion.
    
    [Fun Toolkit]: https://dl.acm.org/doi/pdf/10.1145/1139073.1139096

--- frontend/src/features/challenge/steps/Completed.jsx
+++ frontend/src/features/challenge/steps/Completed.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useLayoutEffect } from "react";
 import { View } from "react-native";
 import { Button, Text } from "react-native-paper";
+import SmileyOMeter from "../../../shared/components/SimleyOMeter";
 import { t } from "../../i18n";
 import ChallengeContext from "../ChallengeContext";
 import styles from "./styles";
@@ -14,14 +15,25 @@ function Completed({ navigation }) {
     });
   }, [navigation, challenge]);
 
+  const handleSmileyPress = (score) => {
+    console.debug(`User rated challenge ${score}`);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.shoutOutContainer}>
         <Text style={styles.shoutOut}>{t("Well done!")}</Text>
-        <Button onPress={() => navigation.navigate("category:list")}>
-          {t("Up for another challenge?")}
-        </Button>
+        <SmileyOMeter
+          message={t("What do you think about the challenge?")}
+          onPress={handleSmileyPress}
+        />
       </View>
+      <Button
+        style={{ padding: 20 }}
+        onPress={() => navigation.navigate("category:list")}
+      >
+        {t("Up for another challenge?")}
+      </Button>
     </View>
   );
 }
---
+++ frontend/src/shared/components/SimleyOMeter.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable global-require */
+import React from "react";
+import { Image, TouchableOpacity, View } from "react-native";
+import { Subheading, Text } from "react-native-paper";
+import { t } from "../../features/i18n";
+import styles from "./SimleyOMeter.style";
+
+const smileys = {
+  awful: {
+    score: -2,
+    label: t("Awful"),
+    asset: require("./assets/smiley-awful.png"),
+  },
+  notGood: {
+    score: -1,
+    label: t("Not very good"),
+    asset: require("./assets/smiley-not-good.png"),
+  },
+  good: {
+    score: 0,
+    label: t("Good"),
+    asset: require("./assets/smiley-good.png"),
+  },
+  reallyGood: {
+    score: 1,
+    label: t("Really good"),
+    asset: require("./assets/smiley-really-good.png"),
+  },
+  brilliant: {
+    score: 2,
+    label: t("Brilliant"),
+    asset: require("./assets/smiley-brilliant.png"),
+  },
+};
+
+function Smiley({ score, label, asset, onPress }) {
+  const handleOnPress = () => {
+    onPress(score);
+  };
+
+  return (
+    <TouchableOpacity style={styles.smileyContainer} onPress={handleOnPress}>
+      <Image source={asset} />
+      <Text style={styles.smileyLabel}>{label}</Text>
+    </TouchableOpacity>
+  );
+}
+
+// A Smiley-o-meter implementation based on the Fun Toolkit from Janet C. Read
+// See: https://dl.acm.org/doi/pdf/10.1145/1139073.1139096
+export default function SmileyOMeter({ message, style, onPress }) {
+  return (
+    <View style={[styles.container, style]}>
+      <Subheading>{message}</Subheading>
+      <View style={styles.smileyometer}>
+        {Object.entries(smileys).map(([key, item]) => (
+          <Smiley
+            key={key}
+            score={item.score}
+            label={item.label}
+            asset={item.asset}
+            onPress={onPress}
+          />
+        ))}
+      </View>
+    </View>
+  );
+}

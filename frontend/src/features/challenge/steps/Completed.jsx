55a4863a0566c233f53b39f8d167c46553a6a3c9
Einar Uvsløkk <einaru@stud.ntnu.no>
2022-02-23 15:13:01 +0100

    Make the challenge smiley-o-meter "pop out" more

--- frontend/src/features/challenge/steps/Completed.jsx
+++ frontend/src/features/challenge/steps/Completed.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useLayoutEffect } from "react";
 import { View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { Button, Surface, Text } from "react-native-paper";
 import { SmileyOMeter } from "../../../shared/components";
 import { t } from "../../i18n";
 import ChallengeContext from "../ChallengeContext";
@@ -44,11 +44,13 @@ function Completed({ navigation }) {
           <Reward title={calculatePoints()} subtitle={t("points")} />
         ) : null}
         <Text style={styles.shoutOut}>{t("Well done!")}</Text>
+      </View>
+      <Surface style={{ margin: 8, elevation: 1 }}>
         <SmileyOMeter
           message={t("What do you think about the challenge?")}
           onPress={handleSmileyPress}
         />
-      </View>
+      </Surface>
       <Button
         style={{ padding: 20 }}
         onPress={() => navigation.navigate("category:list")}

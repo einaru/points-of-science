288a4b2300c67342fd2b951aa0f0292eacc9330f
and <andigernes@gmail.com>
2022-03-01 09:41:01 +0100

    Backend: Asserting Experimental Group
    Implemented function asserting a user is in experimental group to be allowed to use a resource.

--- backend/src/API/GraphQL/assert.js
+++ backend/src/API/GraphQL/assert.js
@@ -13,6 +13,12 @@ export function assertIsAdmin(user) {
   }
 }
 
+export function assertIsExperimental(user) {
+  if(user && user.permission !== config.permissionLevel.experimental) {
+    throw new ForbiddenError("Expermental permission is required.");
+  }
+}
+
 export function assertTextInput(text, inputType) {
   if (text == null) {
     throw new UserInputError(`${inputType} cannot be null.`);

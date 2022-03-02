df81f72ed32b778e2baadb65d1df79c58138e624
Einar Uvsløkk <einaru@stud.ntnu.no>
2022-03-02 14:19:57 +0100

    Remove cachedToken in authLink
    This strategy caused a lot of headache when testing rendering of game
    elements based on permission levels of users. The problem was that the
    cachedToken was not reset on user logout. If a new user logged into the
    same device the cached access token from the previous user was used for
    subsequent requests.
    
    The idea with keeping a cached copy of the token was to limit the number
    of async calls to get the stored access token. We will keep doing async
    calls until a new strategy is implemented.

--- frontend/src/services/api/authLink.js
+++ frontend/src/services/api/authLink.js
@@ -1,20 +1,10 @@
 import { setContext } from "@apollo/client/link/context";
-import { onError } from "@apollo/client/link/error";
 import * as Storage from "../storage";
 import { getNewToken, isTokenValid } from "./token";
 
-// Cached access token to avoid async calls for every request
-// This will be reset upon 401 server error's
-let cachedToken;
-
-const withToken = setContext(() => {
-  if (cachedToken) {
-    return { token: cachedToken };
-  }
-  return Storage.getItem("accessToken").then((accessToken) => {
-    cachedToken = accessToken;
-    return { token: cachedToken };
-  });
+const withToken = setContext(async () => {
+  const accessToken = await Storage.getItem("accessToken");
+  return { token: accessToken };
 });
 
 const withAuth = setContext(async (_, { headers, token }) => {
@@ -26,7 +16,6 @@ const withAuth = setContext(async (_, { headers, token }) => {
 
   if (accessToken) {
     Storage.setItem("accessToken", accessToken);
-    cachedToken = accessToken;
   }
 
   return {
@@ -37,16 +26,6 @@ const withAuth = setContext(async (_, { headers, token }) => {
   };
 });
 
-const resetToken = onError(({ networkError }) => {
-  if (
-    networkError &&
-    networkError.name === "ServerError" &&
-    networkError.statusCode === 401
-  ) {
-    cachedToken = null;
-  }
-});
-
-const authLink = withToken.concat(withAuth).concat(resetToken);
+const authLink = withToken.concat(withAuth);
 
 export default authLink;

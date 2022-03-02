2d5593acf77af09dbd587c8d02c2cb9d0e08343a
Einar Uvsløkk <einar.uvslokk@gmail.com>
2022-03-02 11:26:36 +0100

    Define __DEV__ as ESLint global

--- frontend/src/services/api/errorLink.js
+++ frontend/src/services/api/errorLink.js
@@ -2,7 +2,6 @@
 import { onError } from "@apollo/client/link/error";
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-  // eslint-disable-next-line no-undef
   if (__DEV__) {
     console.debug("GraphQL Errors:", graphQLErrors);
     console.debug("Network Error:", networkError);

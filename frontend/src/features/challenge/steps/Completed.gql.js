3d34ac81f3e0a5295b4284c54cf3e41a64c6fb9a
Einar Uvsløkk <einar.uvslokk@gmail.com>
2022-03-06 22:43:22 +0100

    Update response data for addUserChallenge mutation

--- frontend/src/features/challenge/steps/Completed.gql.js
+++ frontend/src/features/challenge/steps/Completed.gql.js
@@ -3,7 +3,12 @@ import { gql } from "@apollo/client";
 const ADD_USER_CHALLENGE = gql`
   mutation addUserChallenge($data: UserChallengeInput!) {
     addUserChallenge(userChallenge: $data) {
-      message
+      answeredCorrect
+      completed
+      reward {
+        points
+        bonusPoints
+      }
     }
   }
 `;

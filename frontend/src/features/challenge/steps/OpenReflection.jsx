5e30d32048f85cf816eae7c559fe1ce0ddcd24cb
Einar Uvsløkk <einaru@stud.ntnu.no>
2022-03-08 10:37:44 +0100

    Fix keyboard hiding OpenReflection content on iOS
    Fixes #170

--- frontend/src/features/challenge/steps/OpenReflection.jsx
+++ frontend/src/features/challenge/steps/OpenReflection.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
-import { Text, TextInput } from "react-native-paper";
+import { ScrollView, View, Platform, Dimensions } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Text, TextInput, withTheme } from "react-native-paper";
 
 import { HeroImage } from "~shared/components";
 import { t } from "~shared/i18n";
 
 import styles from "./OpenReflection.style";
 
-export default function OpenReflection({ title, onChange }) {
+function OpenReflection({ title, onChange, theme }) {
   const [text, setText] = React.useState("");
 
   const handleChange = (value) => {
@@ -15,20 +16,44 @@ export default function OpenReflection({ title, onChange }) {
     onChange(value);
   };
 
-  return (
-    <ScrollView contentContainerStyle={styles.scroll}>
-      <HeroImage name="science" />
+  const renderContent = () => {
+    const window = Dimensions.get("window");
+    return (
       <View style={styles.content}>
+        <HeroImage name="science" size={window.width - 16} />
         <Text style={styles.title}>{title}</Text>
         <TextInput
-          style={{ flex: 1 }}
           multiline
-          autoFocus
+          style={{ flex: 1 }}
+          autoFocus={Platform.OS !== "ios"}
           label={t("Reflection")}
           value={text}
           onChangeText={handleChange}
         />
       </View>
+    );
+  };
+
+  // Similar to the issue with the form elements in the account package,
+  // we need to be handled iOS devices a bit different here as well.
+  // By using KeyboardAwareScrollView and disabling autofocus (see above),
+  // we have a solution that is not perfect, but works "okay".
+  return Platform.OS === "ios" ? (
+    <KeyboardAwareScrollView
+      contentContainerStyle={[
+        styles.scroll,
+        { backgroundColor: theme.colors.background },
+      ]}
+      resetScrollToCoords={{ x: 0, y: 0 }}
+      scrollEnabled
+    >
+      {renderContent()}
+    </KeyboardAwareScrollView>
+  ) : (
+    <ScrollView contentContainerStyle={styles.scroll}>
+      {renderContent()}
     </ScrollView>
   );
 }
+
+export default withTheme(OpenReflection);

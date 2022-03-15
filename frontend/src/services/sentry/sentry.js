4541bc780026e386edb0fe1a4d88be57c06d4aa5
Einar Uvsløkk <einaru@stud.ntnu.no>
2022-03-15 09:06:43 +0100

    Make Sentry work when running in browser also

---
+++ frontend/src/services/sentry/sentry.js
@@ -0,0 +1,4 @@
+import * as Sentry from "sentry-expo";
+
+export const { ErrorBoundary } = Sentry.Native;
+export default Sentry;
---
+++ frontend/src/services/sentry/sentry.web.js
@@ -0,0 +1,5 @@
+import * as SentryReact from "@sentry/react";
+import * as Sentry from "sentry-expo";
+
+export const { ErrorBoundary } = SentryReact;
+export default Sentry;

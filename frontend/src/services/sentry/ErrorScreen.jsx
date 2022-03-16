f1b1b74709947c1924a91491b0e6a3e7d534a811
Einar Uvsløkk <einaru@stud.ntnu.no>
2022-03-16 16:20:04 +0100

    Optimize assets using expo-optimize
    Moves the ErrorScreen illustration image -> shared/assets and updates
    assetBundlePatterns in app.json.
    
    Unused StorySet illustrations are also removed.
    
    Closes #224

--- frontend/src/services/sentry/ErrorScreen.jsx
+++ frontend/src/services/sentry/ErrorScreen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, View } from "react-native";
 import { Button, Text } from "react-native-paper";
 
+import { illustrations } from "~shared/assets";
 import { t } from "~shared/i18n";
 
 import styles from "./ErrorScreen.style";
@@ -9,7 +10,7 @@ import styles from "./ErrorScreen.style";
 export default function ErrorScreen({ resetError }) {
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("./error.png")} />
+      <Image style={styles.image} source={illustrations.error} />
       <View style={styles.content}>
         <Text style={styles.title}>{t("Something went wrong!")}</Text>
         <Button mode="contained" onPress={resetError}>

f15d016e953814919909fb9f7be7873270e4fd7c
and <andigernes@gmail.com>
2022-04-10 23:31:00 +0200

    Backend: New Config Env Variable, Update README
    New environment variable in config.js file setting the firebase database url.
    
    Added databaseURL field to admin.initializeApp in firestore.js file to make GOOGLE APP ENGINE be able to communicate with Firebase without an error.
    
    Updated README file of project with an explaination of how to set environment variable DATABASE_URL.

--- backend/src/Database/firestore.js
+++ backend/src/Database/firestore.js
@@ -5,6 +5,7 @@ import config from "../Config/config.js";
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
+  databaseURL: config.storage.databaseURL,
   storageBucket: config.storage.bucket,
 });
 
